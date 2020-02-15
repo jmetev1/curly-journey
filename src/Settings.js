57a04fe03bb0df7b5b1dc953cec1a0bd046930c6
Jmetev1 <jmetevier@gmail.com>
2020-02-14 16:27:02 -0800

    fix spacing on settings, build remote

--- src/Settings.js
+++ src/Settings.js
@@ -22,7 +22,7 @@ export default class Settings extends React.Component {
     return (
       <Pane>
         <div>Current User is {user}</div>
-        Setting dev to true will set user to test, maybe
+        <div>Setting dev to true will set user to test, maybe</div>
         {Object.entries(rest).map(([key, value]) => (
           <Label key={key} height={height}>
             {key}
